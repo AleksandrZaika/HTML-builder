540c903d21ffc46f2cbe2cc3331086bcd233a20c
AleksandrZ <saniokzaika@gmail.com>
2024-01-15 16:49:11 +0000

    feat: implement script to read content of text.txt, first task complete

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,3 +3,13 @@ const path = require('path');
 
 const fileLocation = path.join(__dirname, 'text.txt');
 const stream = fs.createReadStream(fileLocation, { encoding: 'utf-8' });
+
+let content = '';
+
+stream.on('data', (chunk) => {
+  content += chunk;
+});
+
+stream.on('end', () => {
+  console.log(content);
+});
