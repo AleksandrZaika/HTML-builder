1cd8cec746bbfebe3c1d3fd417ab0965d3a30c5f
AleksandrZ <saniokzaika@gmail.com>
2024-01-16 21:42:03 +0000

    fix: add option withFileTypes: true in task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,27 +1,30 @@
 const fs = require('fs');
 const path = require('path');
+
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, (err, files) => {
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.error("Error", err);
     return;
   }
 
   files.forEach((file) => {
-    const filePath = path.join(folderPath, file);
+    const filePath = path.join(folderPath, file.name);
 
-    fs.stat(filePath, (statErr, stats) => {
-      if (statErr) {
-        console.error('Error', statErr);
-        return;
-      }
+    if (file.isFile()) {
+      fs.stat(filePath, (statErr, stats) => {
+        if (statErr) {
+          console.error('Error', statErr);
+          return;
+        }
 
-      const fileSize = (stats.size / 1024).toFixed(3);
-      const fileExt = path.extname(file).slice(1);
-      const fileName = path.basename(file, `.${fileExt}`);
+        const fileSize = (stats.size / 1024).toFixed(3);
+        const fileExt = path.extname(file.name).slice(1);
+        const fileName = path.basename(file.name, `.${fileExt}`);
 
-      console.log(`${fileName} - ${fileExt} - ${fileSize}kb`);
-    });
+        console.log(`${fileName} - ${fileExt} - ${fileSize}kb`);
+      });
+    }
   });
 });
