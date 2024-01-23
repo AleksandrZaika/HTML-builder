bb63e95ec6ab4394d83c2f1b9054f20050dacd72
AleksandrZ <saniokzaika@gmail.com>
2024-01-23 14:57:43 +0000

    fix: code fix, remove sync

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const stylesFolder = path.join(__dirname, 'styles');
 const projectFolder = path.join(__dirname, 'project-dist');
 const outputFile = path.join(projectFolder, 'bundle.css');
 
-fs.readdir(stylesFolder, (err, files) => {
-  if (err) {
+fs.readdir(stylesFolder)
+  .then(files => {
+    const cssFiles = files.filter(file => path.extname(file) === '.css');
+    const readFilePromises = cssFiles.map(cssFile => {
+      const filePath = path.join(stylesFolder, cssFile);
+      return fs.readFile(filePath, 'utf8');
+    });
+
+    return Promise.all(readFilePromises);
+  })
+  .then(fileContents => {
+    const bundledStyles = fileContents.join('\n');
+    return fs.writeFile(outputFile, bundledStyles);
+  })
+  .then(() => {
+    console.log('Bundle created.');
+  })
+  .catch(err => {
     console.error('Error', err);
-    return;
-  }
-  const cssFiles = files.filter(file => path.extname(file) === '.css');
-  const stylesArray = [];
-  cssFiles.forEach(cssFile => {
-    const filePath = path.join(stylesFolder, cssFile);
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    stylesArray.push(fileContent);
   });
-
-  const bundledStyles = stylesArray.join('\n');
-
-  fs.writeFileSync(outputFile, bundledStyles);
-
-});
